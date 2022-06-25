076fb71e158227ab642ad18a799dbcde2bc5a664
gargabhisar <garg.abhisar@gmail.com>
2022-06-25 23:04:56 +0530

    Home & Dashboard UI Layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path: 'home', component: HomeComponent,
     children: [
-      { path: '', component: DashboardComponent }
+      { path: 'dashboard', component: DashboardComponent }
     ]
   }
 ];
