3dd4bc97b1c2ee7e8e47e36b2e2ea6cf569af31a
gargabhisar <garg.abhisar@gmail.com>
2022-06-25 17:54:43 +0530

    Login Angular

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  loginForm!: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { 
   }
 
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      useremail: ['', [Validators.required, Validators.email]],
+      userpassword: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    console.log(this.loginForm.value);
+  }
 }
