97759081716e9b40adcf2a60951adaa9b3e94688
gargabhisar <garg.abhisar@gmail.com>
2022-06-26 00:29:30 +0530

    Fixed Content Display Issue

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  host: {
+    class:'classToFixDisplayIssue'
+}
 })
 export class DashboardComponent implements OnInit {
 
